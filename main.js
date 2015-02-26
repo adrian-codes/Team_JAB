144c126c3c1c8acb566f4b87e20eacfe062a70aa
Brian <jibrianh@gmail.com>
2015-02-26 15:25:57 -0800

    Added drop down selection.

--- main.js
+++ main.js
@@ -1,49 +1,24 @@
-    function generateRandomNum() {
-        return Math.floor( ( (Math.random() * 1000000) + 1000000 ) ); 
-    }
-
-    
-    function loadMedia(title) {
-        var _data;
+    // function loadMedia(title) {
         
-        $.ajax({
-            // url: 'http://www.omdbapi.com/?s=' + title +'&tomatoes=true',
 
-            // get random title
-            url: 'http://www.omdbapi.com/?i=tt' + generateRandomNum(),
-            dataType: 'jsonp',
-            success: function(data) {
-             
-
-                $movieContent = $('#movie_content');
-                 
-                $movieContent.find('h4').html(data.Title);
-                $movieContent.find('p').html(data.Plot);
-                
-                // create poster img
-                var poster = document.createElement('img');
-                poster.setAttribute('src', data.Poster);
-                
-
-                $('#poster').html(poster);
-                console.log(data);
-                setTimeout(function() {
-                    $.ajax({
-                    url: 'http://gdata.youtube.com/feeds/api/videos?q=' + data.Title + '+' + data.Year + '+' + 'trailer' + '&format=5&max-results=1&v=2&alt=jsonc',
-                        success: function(data){
-                        var url = data.data.items[0].player['default'];
-                        var url = url.replace("watch?v=", "v/");
-                        var player = '<iframe width="560" height="315" src=' + url + ' frameborder="0" allowfullscreen></iframe>';
+     
+    //             setTimeout(function() {
+    //                 $.ajax({
+    //                 url: 'http://gdata.youtube.com/feeds/api/videos?q=' + data.Title + '+' + data.Year + '+' + 'trailer' + '&format=5&max-results=1&v=2&alt=jsonc',
+    //                     success: function(data){
+    //                     var url = data.data.items[0].player['default'];
+    //                     var url = url.replace("watch?v=", "v/");
+    //                     var player = '<iframe width="560" height="315" src=' + url + ' frameborder="0" allowfullscreen></iframe>';
                          
-                        $('.trailer').html(player)
-                        }, // success
-                    });
-                });
-            } // success
-        });
+    //                     $('.trailer').html(player)
+    //                     }, // success
+    //                 });
+    //             });
+    //         } // success
+    //     });
                 
        
-    } // loadMedia
+    // } // loadMedia
         
     
     
@@ -52,8 +27,22 @@
     });
     
 
+    var movieList = movies.feed.entry;
+    
+    //movieList[i]['im:image'][2].label
+    //movieList[i]['im:name'].label
+    //movieList[i].summary.label
+    //movieList[i].category.attributes.label
 
 
 
 
+    function getGenres(obj) {
+        var genres = [];
 
+        for (var i = 0; i < obj.feed.entry.length; i++) {
+            genres.push(obj.feed.entry[i].category.attributes.label)
+        }
+
+        return genres;
+    }
