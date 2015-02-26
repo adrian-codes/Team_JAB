49c4b076b7cf3305f5453c8b8cc8926d2e9e9d26
Brian <jibrianh@gmail.com>
2015-02-26 14:33:39 -0800

    Brians jQuery AJAX Call

--- main.js
+++ main.js
@@ -1,34 +1,33 @@
 
     function generateRandomNum() {
-        return Math.floor( ( (Math.random() * 1999999) + 1000000 ) ); 
+        return Math.floor( ( (Math.random() * 1000000) + 1000000 ) ); 
     }
 
     
-    function loadMedia() {
+    function loadMedia(title) {
     
         
         $.ajax({
-            url: 'http://www.omdbapi.com/?i=tt' + generateRandomNum(),
+            // url: 'http://www.omdbapi.com/?i=tt' + generateRandomNum(),
+
+            url: 'http://www.omdbapi.com/?s=' + title +'&tomatoes=true',
             dataType: 'jsonp',
             success: function(data) {
                 var $movieContent = $('#movie_content');
                 
-                console.log(data.Poster);
                 
                 $movieContent.find('h4').html(data.Title);
                 $movieContent.find('p').html(data.Plot);
                 
+                // create poster img
+                var poster = document.createElement('img');
+                poster.setAttribute('src', data.Poster);
                 
-                var img = document.createElement('img');
-                
-                img.src = data.Poster;
-                
-                $('#movie_content').append(img);
-                
+
+                $('#poster').html(poster);
+                console.log(data);
             }
         });
-        
-        
         
     }
     
@@ -39,7 +38,24 @@
     });
     
 
-    
+
+    function test(title) {
+
+$.ajax({
+      url: 'http://gdata.youtube.com/feeds/api/videos?q='+title+'&format=5&max-results=1&v=2&alt=jsonc',
+        success: function(data){
+            var url = data.data.items[0].player['default'];
+            var url = url.replace("watch?v=", "v/");
+            var player = '<iframe width="560" height="315" src=' + url + ' frameborder="0" allowfullscreen></iframe>';
+            $('#container').html(player)
+            console.log(data.data.items[0].player['default']);
+        },
+
+    });
+
+}
+
+
                  
             
                  
