1f084193575f1eccc7cf4591f92874a3cbebc1ca
Brian <jibrianh@gmail.com>
2015-02-28 07:30:20 -0800

    Cleaning up js. Created movieGenreId which contains itunes movie genre ids for future us

--- main.js
+++ main.js
@@ -1,88 +1,86 @@
-    // function loadMedia(title) {
-        
+var movieGenreId = {
+    'Action & Adventure': 4401,
+    'Anime': 4402,
+    'Classics': 4403,
+    'Comedy': 4404,
+    'Documentary': 4405,
+    'Drama': 4406,
+    'Foreign': 4407,
+    'Horror': 4408,
+    'Independent': 4409,
+    'Kids & Family': 4410,
+    'Musicals': 4411,
+    'Romance': 4412,
+    'Sci-Fi & Fantasy': 4413,
+    'Short Films': 4414,
+    'Special Interests': 4415,
+    'Thriller': 4416,
+    'Sports': 4417,
+    'Western': 4418,
+    'Urban': 4419,
+    'Holiday': 4420,
+    'Made For TV': 4421,
+    'Concert Films': 4422,
+    'Music Documentaries': 4423,
+    'Music Feature Films': 4424,
+    'Japanese Cinema': 4425,
+    'Jidaigeki': 4426,
+    'Tokusatsu': 4427,
+    'Korean Cinema': 4428
+}  
 
-     
-    //             setTimeout(function() {
-    //                 $.ajax({
-    //                 url: 'http://gdata.youtube.com/feeds/api/videos?q=' + data.Title + '+' + data.Year + '+' + 'trailer' + '&format=5&max-results=1&v=2&alt=jsonc',
-    //                     success: function(data){
-    //                     var url = data.data.items[0].player['default'];
-    //                     var url = url.replace("watch?v=", "v/");
-    //                     var player = '<iframe width="560" height="315" src=' + url + ' frameborder="0" allowfullscreen></iframe>';
-                         
-    //                     $('.trailer').html(player)
-    //                     }, // success
-    //                 });
-    //             });
-    //         } // success
-    //     });
-                
-       
-    // } // loadMedia
-        
+
+
+var movieList = movies.feed.entry;
     
+$('#movie_button').on('click', function(e) {
+    var genreSelection = $('#genre_selector').val();
 
-    var movieList = movies.feed.entry;
-    
-    //movieList[i]['im:image'][2].label
-    //movieList[i]['im:name'].label
-    //movieList[i].summary.label
-    //movieList[i].category.attributes.label
+    var filteredSelection = filterGenre(movieList, genreSelection);
 
+    $('#movie_content').html('');
 
-    $('#movie_button').on('click', function(e) {
-        var genreSelection = $('#genre_selector').val();
-
-        var filteredSelection = filterGenre(movieList, genreSelection);
-
-        $('#movie_content').html('');
-
-        for (var i = 0; i < filteredSelection.length; i++) {
-
-            var html = '<div id=m-' + i + '>' + 
-                        '<div class=movie_top>' +
-                            '<div class=movie_top_left>' +
-                                 '<img src=' + filteredSelection[i]['im:image'][2].label + '>' + 
-                            '</div>' +
-                            '<div class=movie_top_right>' +
-                                '<h4>' + filteredSelection[i]['im:name'].label + '</h4>' + 
-                                '<p>' + filteredSelection[i].summary.label + '</p>' +
-                            '</div>' +
-                            '<div class=trailer></div>' +
-                        '</div>';
-
-
-     
-               
+    for (var i = 0; i < filteredSelection.length; i++) {
+        var html = '<div id=m-' + i + '>' + 
+                    '<div class=movie_top>' +
+                        '<div class=movie_top_left>' +
+                              '<img src=' + filteredSelection[i]['im:image'][2].label + '>' + 
+                        '</div>' +
+                        '<div class=movie_top_right>' +
+                            '<h4>' + filteredSelection[i]['im:name'].label + '</h4>' + 
+                            '<p>' + filteredSelection[i].summary.label + '</p>' +                            '</div>' +
+                        '<div class=trailer></div>' +
+                    '</div>';
             
-            $('#movie_content').append(html); 
-
+        $('#movie_content').append(html); 
+    } // end loop
         
-
-        } // end loop
-        
-
-        for (var i = 0; i < filteredSelection.length; i++) {
-
-            (function() {
-                var sid = i;
+    for (var i = 0; i < filteredSelection.length; i++) {
+        (function() {
+            var sid = i;
             
-                $.ajax({
-                    url: 'http://gdata.youtube.com/feeds/api/videos?q=' + filteredSelection[i]['im:name'].label + '+' + filteredSelection[i]['im:releaseDate'].label.substring(0, 4) + '+' + 'trailer' + '&format=5&max-results=1&v=2&alt=jsonc',
+            $.ajax({
+                url: 'http://gdata.youtube.com/feeds/api/videos?q=' + filteredSelection[i]['im:name'].label + '+' + filteredSelection[i]['im:releaseDate'].label.substring(0, 4) + '+' + 'trailer' + '&format=5&max-results=1&v=2&alt=jsonc',
          
-                    success: function (data){
-                        var url = data.data.items[0].player['default'];
-                        var url = url.replace("watch?v=", "v/");
-                        var player = '<iframe width="560" height="315" src=' + url + ' frameborder="0" allowfullscreen></iframe>';   
-                    
-                        $('#m-' + sid).find('.trailer').append(player);
+                success: function (data){
+                    var url = data.data.items[0].player['default'];
+                    var url = url.replace("watch?v=", "v/");
+                    var player = '<iframe width="560" height="315" src=' + url + ' frameborder="0" allowfullscreen></iframe>';   
+                
+                    $('#m-' + sid).find('.trailer').append(player);
 
-                    }, // success
-                });
-            })(); // closure
-        } // loop
+                }, // success
+            });
+        })(); // closure
+    } // loop
 
-    })
+});
+
+
+
+
+
+
 
 
     function filterGenre(arr, genre) {
